155ca37db50dc61f9e00d15e98169dfffd2336a1
SerBay <79163956434@yandex.ru>
2023-01-03 00:18:08 +0300

    First commit - Create package

---
+++ lib/dart_basics.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_basics;
+
+export 'src/dart_basics_base.dart' show Awesome;
+
+// TODO: Export any libraries intended for clients of this package.
